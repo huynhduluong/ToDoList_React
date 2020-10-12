4e9183fe122fee98c18b268c12d772aafdc1bda5
Huynh Du Luong <huynhduluong@gmail.com>
2020-10-12 14:36:30 +0700

    handle change status and delete todo

---
+++ src/components/layout/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+  headerStyle = {
+    background: "#333",
+    color: "#fff",
+    textAlign: "center",
+    padding: "10px",
+  };
+
+  linkStyle = {
+    color: "#fff",
+    textDecoration: "none",
+  };
+
+  render() {
+    return (
+      <header style={this.headerStyle}>
+        <h1>TodoList</h1>
+        <Link style={this.linkStyle} to="/">
+          Home
+        </Link>{" "}
+        |{" "}
+        <Link style={this.linkStyle} to="/about">
+          About
+        </Link>
+      </header>
+    );
+  }
+}
